3ae29e5bd0d4dcd52071f6122662df7e08c2e3c8
Kevin Shukla <kevn1841@gmail.com>
2016-10-15 04:18:49 -0400

    works

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+   {
+      "name":"Kevin Shukla",
+      "photo":"https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/14581588_1062303043888614_4515063280130379235_n.jpg?oh=2681bea47d9f19e6258944611709d684&oe=58AA6EDE",
+      "scores":[
+         "5",
+         "1",
+         "4",
+         "4",
+         "5",
+         "1",
+         "2",
+         "5",
+         "4",
+         "1"
+      ]
+   },
+   {
+      "name":"",
+      "photo":"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      "scores":[
+         "4",
+         "2",
+         "5",
+         "1",
+         "3",
+         "2",
+         "2",
+         "1",
+         "3",
+         "2"
+      ]
+   }
+]
+
+// Here we export the array. This makes it accessible to other files using require. 
+module.exports = friends;
